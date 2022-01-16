c85f22e5c232d4fbcbc6766cdc7008fdd1bc0d93
Eric Wang <87143658+ewang0@users.noreply.github.com>
2022-01-16 15:44:36 -0500

    Add submit search button with styles

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,26 +1,40 @@
 import React, { useState } from 'react';
 import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import './Search.scss'
 
 const Search = ({ handleSubmit }) => {
-    const searchValue = document.getElementById('search')
     const [searchState, setSearchState] = useState('');
     return(
-        <form>
+        <form className="search-bar">
             <TextField 
                 className="search"
                 //label="Search"
+                hiddenLabel
+                fullWidth
                 id="search"
                 //defaultValue="Search"
-                placeholder="Search by artist, title, department..."
+                placeholder="Search"
                 size="small"
-                color="secondary"
+                variant="filled"
+                //color="secondary"
                 onChange={(event) => setSearchState(event.target.value)}
                 value={searchState}
+                InputProps={{
+                    endAdornment: 
+                        <InputAdornment 
+                            position="end"
+                            onClick={(event) => {
+                                handleSubmit(event, searchState)
+                                setSearchState('')
+                            }}
+                            >
+                                <SearchIcon className="submit-search-button" />
+                        </InputAdornment>,
+                }}
             />
-            <button className="submit-search" onClick={(event) => {
-                handleSubmit(event, searchState)
-                setSearchState('')
-            }}>Search</button>
         </form>
     )
 }
