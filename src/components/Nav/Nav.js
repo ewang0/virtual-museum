1f36449115d32cb7672042927200ba1fa8f66a3f
Eric Wang <87143658+ewang0@users.noreply.github.com>
2022-01-16 15:46:12 -0500

    Style Nav section

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -6,16 +6,18 @@ import Search from '../Search/Search.js'
 const Nav = ({ handleSubmit }) => {
     return (
         <nav>
-            <div>
-                <h1>METVIRTUAL</h1>
-                <div className="nav-links">
-                    <NavLink to="/">Home</NavLink>
-                    <NavLink to="/about">About</NavLink>
-                    <NavLink to="/saved">Saved</NavLink>
+            <div className="nav-content">
+                <div className="logo-nav-container">
+                    <h1 className="logo">METVIRTUAL</h1>
+                    <div className="nav-links">
+                        <NavLink to="/">Home</NavLink>
+                        <NavLink to="/about">About</NavLink>
+                        <NavLink to="/saved">Saved</NavLink>
+                    </div>
                 </div>
+                <Search handleSubmit={handleSubmit} />
+                <p>Search the collection of the Metropolitan Museum of Art and get inspired without leaving home.</p>
             </div>
-            <Search handleSubmit={handleSubmit} />
-            <p>Search the collection of the Metropolitan Museum of Art and get inspired without leaving home.</p>
         </nav>
     )
 }
