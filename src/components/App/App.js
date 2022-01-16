d96d35dae5c70562b2b3dacd1127165a0e8beaac
Eric Wang <87143658+ewang0@users.noreply.github.com>
2022-01-15 19:29:18 -0500

    Add fetch calls and logic to display objects

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,46 @@
+import React, { useEffect, useState } from 'react';
 import './App.scss';
+import Nav from '../Nav/Nav.js'
+import ImageGrid from '../ImageGrid/ImageGrid.js'
 
 function App() {
+  const [objectIDs, setObjectIDs] = useState([]);
+  const [imageURLs, setImageURLs] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async() => {
+        const res = await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=sunflowers`)
+        const resJson = await res.json()
+        .catch(error => console.log(error));
+        setObjectIDs(resJson.objectIDs.splice(0, 10))
+        console.log(resJson.objectIDs) 
+    }
+    fetchData();
+    }, []);
+
+    useEffect(() => {
+      let idArray = [];
+      for(let i=0; i<objectIDs.length; i++) {
+          const fetchData = async() => {
+                  const res = await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectIDs[i]}`)
+                  const resJson = await res.json()
+                  .catch(error => console.log(error));
+                  idArray.push(resJson.primaryImage)
+                  
+                  if(i === objectIDs.length-1) {
+                      setImageURLs(idArray)
+                      console.log(idArray)
+                  }
+              }
+              fetchData();
+      }
+      }, [objectIDs]);
+
+    
   return (
-    <main className="App">
-      <h1>METVIRTUAL</h1>
+    <main className="main-container">
+      <Nav />
+      <ImageGrid imageURLs={imageURLs}/>
     </main>
   );
 }
