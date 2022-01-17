fd18d201ec3d07284ea644b3c21c2e436fc6142d
Eric Wang <87143658+ewang0@users.noreply.github.com>
2022-01-17 12:14:05 -0500

    Create About and Details components

---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './About.scss'
+
+const About = () => {
+    return(
+        <section>
+
+        </section>
+    )
+}
+
+export default About;
---
+++ src/components/Details/Details.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import './Details.scss'
+
+const Details = () => {
+    return(
+        <section>
+            
+        </section>
+    )
+}
+
+export default Details;
