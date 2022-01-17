5799093d3ec0f3b1112ace69019fad3f14770c58
Eric Wang <87143658+ewang0@users.noreply.github.com>
2022-01-17 14:50:56 -0500

    Display carousel

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom'
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
 import './Details.scss'
 
 const Details = () => {
     // const objectID = useParams().objectID;
     //console.log(objectID);
-    const objectID = "459123";
+    const objectID = "2019";
     const [artObject, setArtObject] = useState();
+    const [allImages, setAllImages] = useState([])
 
     useEffect(() => {
         const fetchData = async() => {
@@ -15,11 +18,41 @@ const Details = () => {
             const resJson = await res.json()
             .catch(error => console.log(error));
             setArtObject(resJson)
+            const allObjectImages = [resJson.primaryImageSmall];
+            resJson.additionalImages.forEach((additionalImage) => {
+                allObjectImages.push(additionalImage)
+            })
+            setAllImages(allObjectImages);
+
+
+            //console.log(artObject.primaryImageSmall)
         }
         fetchData();
         }, []);
+    
+    // const getAllImages = () => {
+    //     const allImages = [];
+    //     allImages.push(artObject.primaryImageSmall);
+    //     console.log(artObject.primaryImageSmall)
+    //     artObject.additionalImages.forEach((additionalImage) => {
+    //         allImages.push(additionalImage)
+    //     })
+
+    //     return allImages;
+        
+    // }
+
+    const allObjectImages = allImages.map((imageURL) => {
+        return(
+            <img key={artObject.objectID} src={imageURL} alt={artObject.title} />
+            
+        )
+    })
     return(
         <section className="details-section-container">
+            <Carousel className="carousel-container" showStatus={false} showIndicators={false} centerMode={false}>
+                {allObjectImages}
+            </Carousel>
             
         </section>
     )
