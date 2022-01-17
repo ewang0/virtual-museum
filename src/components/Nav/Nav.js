c20a5abd0a10ab8f63e4ef4ed9fc57384f116a5a
Eric Wang <87143658+ewang0@users.noreply.github.com>
2022-01-17 10:21:13 -0500

    Clear check inputs on search submit (refactor Nav.js)

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Nav.scss'
 import { NavLink } from 'react-router-dom'
 import Search from '../Search/Search.js'
 import Checkbox from '@mui/material/Checkbox';
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+import SearchIcon from '@mui/icons-material/Search';
+import { ResetTv } from '@mui/icons-material';
 
 const Nav = ({ handleSubmit, handleChecked }) => {
+    const [searchState, setSearchState] = useState('');
+    const [checkedState, setCheckedState] = useState({isOnView: false, isHighlight: false});
+
     return (
         <nav>
             <div className="nav-content">
@@ -17,15 +24,43 @@ const Nav = ({ handleSubmit, handleChecked }) => {
                     </div>
                 </div>
                 <p>Search the collection of the Metropolitan Museum of Art and get inspired without leaving home.</p>
-                <Search handleSubmit={handleSubmit} />
+                {/* <Search handleSubmit={handleSubmit} /> */}
+                <form className="search-bar">
+                    <TextField 
+                        className="search"
+                        hiddenLabel
+                        fullWidth
+                        id="search"
+                        placeholder="Search"
+                        size="small"
+                        variant="filled"
+                        onChange={(event) => setSearchState(event.target.value)}
+                        value={searchState}
+                        InputProps={{
+                            endAdornment: 
+                                <InputAdornment 
+                                    position="end"
+                                    onClick={(event) => {
+                                        handleSubmit(event, searchState)
+                                        setSearchState('')
+                                        setCheckedState({isOnView: false, isHighlight: false});
+                                    }}
+                                    >
+                                        <SearchIcon className="submit-search-button" />
+                                </InputAdornment>,
+                        }}
+                    />
+                </form>
                 <form className="search-filter-container">
                     <p>Filters:</p>
                     <label>
                         <Checkbox 
                             id="onView"
+                            checked={checkedState.isOnView}
                             onChange={(event) => {
                                 console.log(event.target.id)
                                 handleChecked(event.target.checked, event.target.id)
+                                setCheckedState({isOnView: event.target.checked, isHighlight: checkedState.isHighlight})
                             }}
                         />
                         On view
@@ -33,9 +68,11 @@ const Nav = ({ handleSubmit, handleChecked }) => {
                     <label>
                         <Checkbox 
                             id="isHighlight"
+                            checked={checkedState.isHighlight}
                             onChange={(event) => {
                                 console.log(event.target.id)
                                 handleChecked(event.target.checked, event.target.id)
+                                setCheckedState({isOnView: checkedState.isOnView, isHighlight: event.target.checked})
                             }}
                         />
                         Highlighted
