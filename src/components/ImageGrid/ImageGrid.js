5b133fa014d55722e09a234389a45135f46274f5
Eric Wang <87143658+ewang0@users.noreply.github.com>
2022-01-17 18:01:05 -0500

    Refactor ImageGrid with PropTypes

--- src/components/ImageGrid/ImageGrid.js
+++ src/components/ImageGrid/ImageGrid.js
@@ -1,29 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import './ImageGrid.scss'
-import ImageGridHeader from '../ImageGridHeader/ImageGridHeader'
-import { Link } from 'react-router-dom'
+import './ImageGrid.scss';
+import ImageGridHeader from '../ImageGridHeader/ImageGridHeader';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 const ImageGrid = ({ displayedArtObjects, handleSort }) => {
-    // const [imageURLs, setImageURLs] = useState([]);
-
-    // useEffect(() => {
-    //     let idArray = [];
-    //     for(let i=0; i<objectIDs.length; i++) {
-    //         const fetchData = async() => {
-    //                 const res = await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectIDs[i]}`)
-    //                 const resJson = await res.json()
-    //                 .catch(error => console.log(error));
-    //                 idArray.push(resJson.primaryImage)
-                    
-    //                 if(i === objectIDs.length-1) {
-    //                     setImageURLs(idArray)
-    //                     console.log(idArray)
-    //                 }
-    //             }
-    //             fetchData();
-    //     }
-    //     }, []);
-
     const images = displayedArtObjects.map((artObject) => {
         return(
             <Link to={`/details/${artObject.objectID}`}>
@@ -43,3 +24,8 @@ const ImageGrid = ({ displayedArtObjects, handleSort }) => {
 }
 
 export default ImageGrid;
+
+ImageGrid.propTypes = {
+    displayedArtObjects: PropTypes.array,
+    handleSort: PropTypes.func
+};
