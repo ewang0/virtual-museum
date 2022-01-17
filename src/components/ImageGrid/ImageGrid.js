555e82ba74b1aff1d8478b5620d30be0e5c1d9b4
Eric Wang <87143658+ewang0@users.noreply.github.com>
2022-01-17 18:03:57 -0500

    Add key prop to Links

--- src/components/ImageGrid/ImageGrid.js
+++ src/components/ImageGrid/ImageGrid.js
@@ -7,7 +7,7 @@ import PropTypes from 'prop-types';
 const ImageGrid = ({ displayedArtObjects, handleSort }) => {
     const images = displayedArtObjects.map((artObject) => {
         return(
-            <Link to={`/details/${artObject.objectID}`}>
+            <Link key={artObject.objectID} to={`/details/${artObject.objectID}`}>
                 <img className="" key={artObject.objectID} src={artObject.primaryImageSmall} alt={artObject.title} />
             </Link>
         )
