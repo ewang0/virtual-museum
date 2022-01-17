2a52e30747410f23b10e51b730f41dfa04fffaf2
Eric Wang <87143658+ewang0@users.noreply.github.com>
2022-01-17 13:34:05 -0500

    Fetch sample data for Details page

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,9 +1,25 @@
 import React, { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom'
 import './Details.scss'
 
 const Details = () => {
+    // const objectID = useParams().objectID;
+    //console.log(objectID);
+    const objectID = "459123";
+    const [artObject, setArtObject] = useState();
+
+    useEffect(() => {
+        const fetchData = async() => {
+            const res = await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectID}`)
+            console.log('details fetch:', `https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectID}`)
+            const resJson = await res.json()
+            .catch(error => console.log(error));
+            setArtObject(resJson)
+        }
+        fetchData();
+        }, []);
     return(
-        <section>
+        <section className="details-section-container">
             
         </section>
     )
