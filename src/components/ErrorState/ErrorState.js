d3dd1dd4fefb60c12a339b0c94bf943f77197fbe
Eric Wang <87143658+ewang0@users.noreply.github.com>
2022-01-18 15:11:09 -0500

    Create no results state for department sort

--- src/components/ErrorState/ErrorState.js
+++ src/components/ErrorState/ErrorState.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './ErrorState.scss';
 
-const ErrorState = ({ error }) => {
+const ErrorState = ({ error, type }) => {
     return(
-        <section className="error-state-container">
-            <div className="error-state-content">
+        <section className={`error-state-container ${type}`}>
+            <div className={`error-state-content ${type}`}>
                 <h2>No Results</h2>
                 <p>Looks like no results came back for your search. Please try a different search.</p>
             </div>
